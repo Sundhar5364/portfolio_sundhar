32675e0fee0e56943d8d8995ce07895bed3a7d5c
Sundhar5364 <sundharaaa06@gmail.com>
2024-10-18 12:05:17 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+   
+       <h1> SUNDHAR A</h1><h3>Web Developer</h3>
+       
+           
+            <hr/>
+          
+                <hr/>
+                
+            <h2><b>CONTACT US</b></h2>
+             <div class="cnt">
+                 <ul class="list1"><li>sundharaaa06@gmail.com</li>
+                  <li>Mobile No:8122229511</li>
+                  <li>Address:uthangarai, krishnagiri`</li>
+                </ul><hr/>
+             </div> <h2><b>ABOUT ME</b></h2>
+           <center><p>Passionate and detail oriented <b>web developer </b>with a strong foundation in html technology,recently graduated with a degree in 
+                [BE CSE] from [Anna University],equiped with hands on experience in html. </p>
+             </center>
+             <hr/>
+             <h2> <b>TECHNICAL SKILLS</b></h2>
+            <div class="tns">
+                 <ul class="list1">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JAVASCRIPT</li>
+                <li>PYTHON</li>
+            </ul>
+            </div>
+            <hr/>
+            <h2><b>CERTIFICATION</b></h2>
+            <ul class="list1">
+            <li>Fullstack Web development</li>
+            <li>Inetrnet Of Things</li>
+            <li>Artificial Intelligence</li>
+           </ul> 
+           <hr/>
+           <h2><b>EXTRA CURICULAR ACTIVITY</b></h2>
+           <ul class="list1">
+            <li>Cricket</li>
+            <li>Kabaddi</li>
+            <li>Football</li>
+           </ul>
+
+    </div>
+  );
+}
+
+export default App;
